5b101dc2c6e92033e2d93576c61232f3627c772b
Sivakumar Sadasivam <siva.sk@outlook.com>
2021-02-15 00:08:40 +0530

    Initial Commit

---
+++ src/main/java/com/sk/store/storeanalytics/StoreAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.sk.store.storeanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAnalyticsApplication.class, args);
+	}
+
+}
